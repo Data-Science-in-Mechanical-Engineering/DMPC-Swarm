0210d3e2f2387803723141dde04628b7565087c9
Alexander Graefe <alexander.graefe@dsme.rwth-aachen.de>
2024-01-11 13:40:12 +0100

    temp

--- cps_testbed_python/run_hpc.sh
+++ cps_testbed_python/run_hpc.sh
@@ -3,12 +3,12 @@
 #SBATCH --job-name=eval_dmpc
 #SBATCH --output=/home/mf724021/hpc_data/slurm_output/%A_%a.out
 #SBATCH --account=rwth1483
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=6
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=alexander.graefe@dsme.rwth-aachen.de
 #SBATCH --mem-per-cpu=1GB # memory per node
-#SBATCH --time=0-10:00:00
-#SBATCH --array=0-10
+#SBATCH --time=0-00:10:00
+#SBATCH --array=0-1
 
 module load GCCcore/.9.3.0
 module load Python/3.8.2
