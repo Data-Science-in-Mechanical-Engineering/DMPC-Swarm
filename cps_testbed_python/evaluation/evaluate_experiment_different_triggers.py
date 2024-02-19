9789fe2d0f86972168521a63091c35a2c3f7e146
Alexander Graefe <alexander.graefe@dsme.rwth-aachen.de>
2024-02-19 12:46:31 +0100

    added plotter

--- cps_testbed_python/evaluation/evaluate_experiment_different_triggers.py
+++ cps_testbed_python/evaluation/evaluate_experiment_different_triggers.py
@@ -6,12 +6,52 @@ from pathlib import Path
 
 import pandas as pd
 
+import pickle
+
 
 if __name__ == "__main__":
     ignore_message_loss = True
     num_drones = 16
-    folder_name = "simulation_results/dmpc/demo_visitors"   # hpc_runs/DMPC_MESSAGE_LOSS"
-    path = os.path.dirname(os.path.abspath(__file__)) + f"/../../../{folder_name}/"
+    name = "demo"
+    targets = "../../../experiment_measurements/drone_trajectory_logger_testbed_experiment_demo.p"
+
+    with open(targets, 'rb') as handle:
+        targets = pickle.load(handle)
+
+    trajectories = "../../../experiment_measurements/ExperimentDemo.pickle"
+    pos = None
+    with open(trajectories, 'rb') as handle:
+        data = pickle.load(handle)
+
+    pos = data["logger_pos"]
+    time_stamps = data["time"]
+
+    dists = []
+    dists2 = []
+
+    for drone_id in range(1, 17):
+        current_target = targets[drone_id][list(targets[drone_id].keys())[0]][2]
+        current_target_time = targets[drone_id][list(targets[drone_id].keys())[0]][0]
+        current_target_idx = 0
+        dists.append([])
+        dists2.append([])
+        for t in range(len(pos[drone_id-1])):
+            while time_stamps[t] >= current_target_time:
+                current_target_idx += 1
+                current_target_time = targets[drone_id][list(targets[drone_id].keys())[current_target_idx]][0]
+            current_target = targets[drone_id][list(targets[drone_id].keys())[current_target_idx]][2]
+            dists[-1].append(np.linalg.norm(pos[drone_id-1][t] - current_target))
+            dists2[-1].append(np.linalg.norm(targets[drone_id][list(targets[drone_id].keys())[current_target_idx]][1][0:3] - current_target))
+    time_stamps = np.array(time_stamps) - time_stamps[0]
+    for d in dists:
+        plt.plot(time_stamps, d)
+
+    for d in dists2:
+        plt.plot(time_stamps, d)
+
+    plt.show()
+
+    """path = os.path.dirname(os.path.abspath(__file__)) + f"/../../../{folder_name}/"
 
     plot_states = False
     files = [os.path.join(path, f) for f in os.listdir(path) if f.startswith(f"simulation_result-{num_drones}_drones_simnr_")]
@@ -27,4 +67,4 @@ if __name__ == "__main__":
                 #print(result["target_pos"][j])
                 d.append(np.linalg.norm(result["target_pos"][j][i+1] - result[f"state_{i}"][j][0:3]))
             plt.plot(d)
-        plt.show()
+        plt.show()"""
