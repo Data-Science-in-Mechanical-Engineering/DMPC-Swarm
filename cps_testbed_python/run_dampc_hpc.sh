7abe9d761c1a4bb13d870417a73a37496ed2f2fd
Alexander Graefe <alexander.graefe@dsme.rwth-aachen.de>
2024-03-22 15:28:14 +0100

    temp

--- cps_testbed_python/run_dampc_hpc.sh
+++ cps_testbed_python/run_dampc_hpc.sh
@@ -2,7 +2,9 @@
 #
 #SBATCH --job-name=dampc
 #SBATCH --output=/home/mf724021/hpc_data/slurm_output/%A_%a.out
-#SBATCH --account=rwth1483
+#SBATCH --partition=c23gputest
+#SBATCH --account=supp0006
+# #SBATCH --account=rwth1483
 #SBATCH --cpus-per-task=48
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=alexander.graefe@dsme.rwth-aachen.de
