28d2790a1f15bb78b09ac8d77da7ad690d2ab63f
mf724021 <mf724021@login23-1.hpc.itc.rwth-aachen.de>
2024-08-16 08:42:27 +0200

    temp

--- cps_testbed_python/run_hpc.sh
+++ cps_testbed_python/run_hpc.sh
@@ -1,14 +1,15 @@
 #!/usr/local_rwth/bin/zsh
 #
 #SBATCH --job-name=eval_dmpc
-#SBATCH --output=/home/mf724021/hpc_data/slurm_output/%A_%a.out
+#SBATCH --output=/work/mf724021/slurm_output/%A_%a.out
 #SBATCH --account=rwth1483
 #SBATCH --cpus-per-task=8
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=alexander.graefe@dsme.rwth-aachen.de
 #SBATCH --mem-per-cpu=2700MB # memory per node
 #SBATCH --time=0-23:00:00
-#SBATCH --array=0-528
+#SBATCH --array=0-527
+# #SBATCH --partition=c23mm
 
 module load GCCcore/.9.3.0
 module load Python/3.8.2
