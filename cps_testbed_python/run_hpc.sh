b413784573b9391e02e6dd52951010688566bfa4
mf724021 <mf724021@login23-1.hpc.itc.rwth-aachen.de>
2024-08-23 09:46:14 +0200

    woprking on cluster again

--- cps_testbed_python/run_hpc.sh
+++ cps_testbed_python/run_hpc.sh
@@ -7,11 +7,11 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=alexander.graefe@dsme.rwth-aachen.de
 #SBATCH --time=4-00:00:00
-#SBATCH --array=0-44
+#SBATCH --array=0-528
 
 module load GCCcore/.9.3.0
 module load Python/3.8.2
 
 source venv/bin/activate
-python dmpc_simulation_caller.py -i $SLURM_ARRAY_TASK_ID -n COMPARISON_DMPC_MLR_DMPC
+python dmpc_simulation_caller.py -i $SLURM_ARRAY_TASK_ID -n COMPARISON_TRIGGERS
 deactivate
